0a2c6e00ca8265b03c189223e1e2c23ecc59afae
Sergey <dokxus@mail.ru>
2023-07-31 14:16:05 +0900

    Update tests.py

--- hw01/tests.py
+++ hw01/tests.py
@@ -1,27 +1,24 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import unittest
-from worker import is_gzip_file, parse_log_record
+from worker import load_config, generate_report, parse_log_record
 
+class TestWorkerMethods(unittest.TestCase):
 
-class TestIsGzipFileFunction(unittest.TestCase):
-    def test_is_gzip_file__xml(self):
-        self.assertEqual(is_gzip_file("testpath/file.xml"), False)
+    def test_load_config(self):
+        # Предполагая, что файл с именем config.json существует в текущей директории
+        config = load_config('config.json')
+        self.assertIsInstance(config, dict)
 
-    def test_is_gzip_file__txt(self):
-        self.assertEqual(is_gzip_file("testpath/file.txt"), False)
+    def test_generate_report(self):
+        records = [('url1', 1), ('url2', 2)]
+        data = generate_report(records, 2)
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 2)
 
-    def test_is_gzip_file__gzip(self):
-        self.assertEqual(is_gzip_file("testpath/file.gz"), True)
-
-
-class TestParseRecord(unittest.TestCase):
-    def test_parse_record(self):
-        str = b'1.168.65.96 -  - [29/Jun/2017:03:50:23 +0300] "GET /api/v2/internal/banner/24288647/info HTTP/1.1" 200 351 "-" "-" "-" "1498697423-2539198130-4708-9752780" "89f7f1be37d" 0.072\n'
-        self.assertEqual(parse_log_record(
-            str), ("/api/v2/internal/banner/24288647", "0.072\n"))
-
+    def test_parse_log_record(self):
+        line = b'1.196.116.32 -  - [29/Jun/2017:03:50:22 +0300] "GET /api/v2/banner/24987703 HTTP/1.1" 200 883 "-" "Lynx/2.8.8dev.9 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/2.10.5" "-" "1498697422-2190034393-4708-9752753" "dc7161be3" 0.726'
+        url, duration = parse_log_record(line)
+        self.assertEqual(url, '/api/v2/banner/24987703')
+        self.assertEqual(duration, '0.726')
 
 if __name__ == '__main__':
     unittest.main()
