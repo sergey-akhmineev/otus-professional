dcae470c0984d6501c8b07226a3e29b20c27bb00
Sergey <dokxus@mail.ru>
2023-07-31 16:26:09 +0900

    Update tests.py

--- hw01/tests.py
+++ hw01/tests.py
@@ -15,12 +15,10 @@ class TestWorkerMethods(unittest.TestCase):
         self.assertEqual(len(data), 2)
 
     def test_parse_log_record(self):
-        line = (
-            b'1.196.116.32 -  - [29/Jun/2017:03:50:22 +0300] "GET /api/v2/banner/'
-            b'24987703 HTTP/1.1" 200 883 "-" "Lynx/2.8.8dev.9 libwww-FM/2.14 '
-            b'SSL-MM/1.4.1 GNUTLS/2.10.5" "-" "1498697422-2190034393-4708-9752753"'
-            b'"dc7161be3" 0.726'
-            )
+        line = b'1.196.116.32 -  - [29/Jun/2017:03:50:22 +0300] "GET /api/v2/banner/24987703 HTTP/1.1" 200 883 "-" "Lynx/2.8.8dev.9 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/2.10.5" "-" "1498697422-2190034393-4708-9752753" "dc7161be3" 0.726'
+        url, duration = parse_log_record(line)
+        self.assertEqual(url, '/api/v2/banner/24987703')
+        self.assertEqual(duration, '0.726')
 
         url, duration = parse_log_record(line)
         self.assertEqual(url, '/api/v2/banner/24987703')
