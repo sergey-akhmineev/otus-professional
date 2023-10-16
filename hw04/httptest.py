e6257d18df6040e23ebc90de14cbf4d2c4e0df6f
Sergey <dokxus@mail.ru>
2023-10-16 15:33:56 +0900

    Корректировка

--- hw04/httptest.py
+++ hw04/httptest.py
@@ -13,7 +13,7 @@ import unittest
 
 class HttpServer(unittest.TestCase):
   host = "localhost"
-  port = 80
+  port = 8080
 
   def setUp(self):
     self.conn = httplib.HTTPConnection(self.host, self.port, timeout=10)
